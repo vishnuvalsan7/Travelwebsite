70a10ca52a69a20ffae18926476dd40a683930ce
vishnuvalsan7 <vishnuvalsan.7592@gmail.com>
2023-10-16 04:53:13 +0530

    tarvel

---
+++ credentials/views.py
@@ -0,0 +1,60 @@
+from urllib import request
+
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+# from django.http import request
+from django.shortcuts import render, redirect
+
+
+# Create your views here.
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'invalid credentials')
+            return redirect('login')
+    return render(request, 'login.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        email = request.POST['email']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+        if password == password1:
+            if User.objects.filter(username=username).exists():
+                messages.info(request, "user name already taken")
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, "email already taken")
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, password=password, first_name=first_name,
+                                                last_name=last_name, email=email)
+
+            user.save();
+            return redirect('login')
+        else:
+            messages.info(request, 'password not matched')
+            return redirect('register')
+        return redirect('/')
+    return render(request, "register.html")
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
---
+++ travelapp/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from travelapp import views
+
+urlpatterns = [
+    path('',views.demo,name='demo')
+]
